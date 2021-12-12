adccc83ef7ea93c3823015e2f51d19e0d45cd869
dmrock <equi.denis@gmail.com>
2021-12-12 19:34:58 +0300

    browser execute example added

--- test/specs/upload.js
+++ test/specs/upload.js
@@ -17,4 +17,21 @@ describe("Upload tests", () => {
     // assertion
     await expect($("h3")).toHaveText("File Uploaded!")
   })
+
+  it.only("Upload on a hidden input field", async () => {
+    await browser.url("/cart")
+
+    const filePath = path.join(__dirname, "../data/wdio-logo.jpeg")
+    const remoteFilePass = await browser.uploadFile(filePath)
+
+    // remove hidden class
+    await browser.execute(`document.querySelector('#upload_1').className = ''`)
+
+    await $("#upfile_1").setValue(remoteFilePass)
+    await $("#upload_1").click()
+
+    await expect($("#wfu_messageblock_header_1_label_1")).toHaveTextContaining(
+      "uploaded successfully"
+    )
+  })
 })
