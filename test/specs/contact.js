50ae0927cbcd2694d8d29fb76109fda2cd1fa1cb
dmrock <equi.denis@gmail.com>
2021-12-17 21:59:21 +0300

    randomize form data

--- test/specs/contact.js
+++ test/specs/contact.js
@@ -1,14 +1,15 @@
 import ContactPage from "../pages/contact-page"
+import * as faker from "faker"
 
 describe("Contact form", () => {
   it("Fill the form, submit & assert the success message", async () => {
     await ContactPage.open()
 
     await ContactPage.submitForm(
-      "Ivan",
-      "i.ivanov@gmail.com",
-      "+79687777777",
-      "Some text message..."
+      faker.name.findName(),
+      faker.internet.email(),
+      faker.phone.phoneNumber(),
+      faker.lorem.paragraphs(2)
     )
 
     const successAlert = ContactPage.alertSuccess
