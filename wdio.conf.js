855c03596e62f1a14ee8d2ea913dc50fd35cbb9c
dmrock <equi.denis@gmail.com>
2021-12-18 19:15:24 +0300

    exclude selected tests

--- wdio.conf.js
+++ wdio.conf.js
@@ -24,6 +24,8 @@ exports.config = {
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
+    "./test/specs/**/nav.js",
+    "./test/specs/**/contact.js",
   ],
   // Define suites
   suites: {
